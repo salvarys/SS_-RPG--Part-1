682f0902997cc0f8a4ba55352cb2eb116e921682
salvarys <123325469+salvarys@users.noreply.github.com>
2023-12-14 01:54:04 -0700

    Updated/Added WebGL build

---
+++ SS_RPGpt1/Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    void Update()
+    {
+        // does the player exist?
+        if (PlayerController.me != null && !PlayerController.me.dead)
+        {
+            Vector3 targetPos = PlayerController.me.transform.position;
+            targetPos.z = -10;
+            transform.position = targetPos;
+        }
+    }
+}
